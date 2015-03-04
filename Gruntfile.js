0453935053dc607b6bb29af71884d010df693a9c
Will Shown <w@willshown.com>
2015-03-03 19:45:44 -0800

    Adding templates.

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+var path = require('path'),
+    fs = require('fs'),
+    os = require('os');
+
+module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt);
+
+  var rjs = os.platform() === 'win32' ? 'r.js.cmd' : 'r.js',
+      npm = os.platform() === 'win32' ? 'npm.cmd' : 'npm';
+
+  var assets = {};
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    copy: {
+      templates: {
+        expand: true,
+        src: [
+          './src/templates/*.liquid',
+          './src/templates/**/*.liquid'
+        ],
+        dest: './dist/',
+        rename: function(dest, src){
+          var components = src.split('/');
+          components.splice(0, 3);
+          return dest + components.join('').replace(/\.liquid$/, '.html');
+        }
+      },
+      templates_wide: {
+        expand: true,
+        flatten: true,
+        cwd: './dist/',
+        src: ['pages_show_*', 'user_sessions_*'],
+        dest: './dist/',
+        rename: function(dest, src){
+          return dest + src.replace('.html', '_wide.html');
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['dist']);
+  grunt.registerTask('dist', ['dist:copy']);
+  grunt.registerTask('dist:copy', ['copy:templates', 'copy:templates_wide']);
+
+};
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# upgrade-seattle
-Upgrade Seatte's website
+# Upgrade Seattle
+
+A fork of a NationBuilder theme built for Upgrade Seattle.
+
+Intellectual property for the theme on which this is based belongs to NationBuilder. Upgrade Seattle claims no intellectual property in this project.
